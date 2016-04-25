85b302a7289870f7c043156018edaeddc9b5c8bd
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-04-25 03:46:34 -0300

    Detecção e conexão com hyperlinks funcional, entretanto o sistema de save/load simplesmente parou de funcionar.

--- Notepad18/src/notepad18/app/NotepadTextArea.java
+++ Notepad18/src/notepad18/app/NotepadTextArea.java
@@ -7,6 +7,9 @@ package notepad18.app;
 
 import java.awt.Font;
 import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import javax.swing.text.html.*;
 
 /**
  *
@@ -16,6 +19,8 @@ import javax.swing.*;
  * 
  */
 public class NotepadTextArea extends JTextPane{
+    
+    boolean inProgress = false;
     
     public NotepadTextArea(){
         super();
@@ -27,6 +32,55 @@ public class NotepadTextArea extends JTextPane{
     
     public void append (String s){
         setText (getText() + s);
+    }
+    
+    public void initListener() {
+        getDocument().addDocumentListener(new DocumentListener(){
+            public void insertUpdate(DocumentEvent docEv) {
+                final DocumentEvent e=docEv;
+                SwingUtilities.invokeLater(new Runnable() {
+                    public void run() {
+                        if (e.getDocument() instanceof HTMLDocument
+                                && e.getOffset()>0
+                                && e.getLength()==1
+                                && !inProgress) {
+                            try {
+
+                                HTMLDocument doc = (HTMLDocument)e.getDocument();
+                                String text = doc.getText(e.getOffset(), e.getLength());
+                                
+                                if (text.charAt(0)==' ' || text.charAt(0)=='\n' || text.charAt(0)=='\t') {
+                                    int start = Utilities.getWordStart(NotepadTextArea.this, e.getOffset()-1);
+                                    text=doc.getText(start, e.getOffset()-start);
+                                    
+                                    if (text.startsWith("www")) {
+                                        inProgress=true;
+                                        HTMLEditorKit kit=(HTMLEditorKit)getEditorKit();
+                                        
+                                        SimpleAttributeSet sas = new SimpleAttributeSet();
+                                        sas.addAttribute("EASTER", "EGG");
+                                        doc.setCharacterAttributes(start, text.length(), sas, false);
+ 
+                                        Element elem=doc.getCharacterElement(start);
+                                        String html = "<a href=\"http://" + text + "\">" + text + "</a>";
+                                        doc.setOuterHTML(elem, html);
+                                        inProgress=false;
+                                    }
+                                }
+                                
+                            } catch (Exception exc) {
+                                exc.printStackTrace();
+                            }
+                        }
+                    }
+                });
+            }
+            
+            public void removeUpdate(DocumentEvent e) {
+            }
+            public void changedUpdate(DocumentEvent e) {
+            }
+        });
     }
     
 }
--- Notepad18/src/notepad18/app/NotepadWindow.java
+++ Notepad18/src/notepad18/app/NotepadWindow.java
@@ -7,9 +7,19 @@ package notepad18.app;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Desktop;
 import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.html.HTMLEditorKit;
 import notepad18.buttons.NotepadMenu;
 
 /**
@@ -27,8 +37,15 @@ public class NotepadWindow extends JFrame{
     int HEIGHT = 640;
     int WIDTH = 480;
     
+    boolean changeCursor = true;
+    
     private NotepadMenu menu = new NotepadMenu();
-    private NotepadTextArea textArea = new NotepadTextArea();
+    private NotepadTextArea textArea = new NotepadTextArea(){
+        public void setCursor (Cursor cursor){
+            if (changeCursor)
+                super.setCursor(cursor);
+        }
+    };
     private JScrollPane scrollArea;
     
     public NotepadWindow(){
@@ -39,6 +56,26 @@ public class NotepadWindow extends JFrame{
         scrollArea = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         this.getContentPane().setLayout(new BorderLayout()); // the BorderLayout bit makes it fill it automatically
 	this.getContentPane().add(scrollArea);
+        
+        textArea.setEditorKit(new MyHTMLEditorKit());
+        textArea.setContentType("text/html");
+        textArea.initListener();
+        
+        textArea.addHyperlinkListener(new HyperlinkListener(){
+            public void hyperlinkUpdate(HyperlinkEvent e){
+                
+                if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED){
+                    if (Desktop.isDesktopSupported()){
+                        try{
+                            Desktop.getDesktop().browse(e.getURL().toURI());
+                        } catch (Exception exc){
+                            exc.printStackTrace();
+                        }
+                    }
+                }
+                
+            }
+        });
         
         menu.initialize(this);
         this.setMenuBar(menu);
@@ -124,5 +161,69 @@ public class NotepadWindow extends JFrame{
         textArea.setBackground(Color.white);
         textArea.setFont(new Font("Times New Roman", Font.BOLD, 12));
         textArea.setForeground(Color.black);
+    }
+    
+    private class MyHTMLEditorKit extends HTMLEditorKit{
+        
+        MyLinkController handler = new MyLinkController();
+        
+        public void install(JEditorPane editorPane){
+            MouseListener[] mouseListeners = editorPane.getMouseListeners();
+            MouseMotionListener[] mouseMotionListeners = editorPane.getMouseMotionListeners();
+            
+            super.install(editorPane);
+            
+            for (MouseListener ml: editorPane.getMouseListeners()){
+                editorPane.removeMouseListener(ml);
+            }
+            
+            for (MouseListener ml: mouseListeners){
+                editorPane.addMouseListener(ml);
+            }
+            
+            for (MouseMotionListener mml: editorPane.getMouseMotionListeners()){
+                editorPane.removeMouseMotionListener(mml);
+            }
+            
+            for (MouseMotionListener mml: mouseMotionListeners){
+                editorPane.addMouseMotionListener(mml);
+            }
+            
+            editorPane.addMouseListener(handler);
+            editorPane.addMouseMotionListener(handler);
+        }
+        
+    }
+    
+    private class MyLinkController extends HTMLEditorKit.LinkController{
+        
+        public void mouseClicked (MouseEvent e){
+            JEditorPane editorPane = (JEditorPane) e.getSource();
+            
+            if (editorPane.isEditable() && SwingUtilities.isLeftMouseButton(e)){
+                if (e.getClickCount()==1){
+                    editorPane.setEditable(false);
+                    super.mouseClicked(e);
+                    editorPane.setEditable(true);
+                }
+            }
+        }
+        
+        public void mouseMoved (MouseEvent e){
+            JEditorPane editorPane = (JEditorPane) e.getSource();
+            
+            if (editorPane.isEditable()){
+                changeCursor = false;
+                editorPane.setEditable(false);
+                changeCursor = true;
+                
+                super.mouseMoved(e);
+                
+                changeCursor = false;
+                editorPane.setEditable(true);
+                changeCursor = true;
+            }
+        }
+        
     }
 }
